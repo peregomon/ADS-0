a47b146b71eca0d01529e05bd51f8b3cf7f7bf1a
peregomon <160238970+peregomon@users.noreply.github.com>
2024-03-01 10:41:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int gcd(int a, int b) {
     int c = 1;
     int z = (a > b) ? a : b;
     for (int i = 1; i <= z; ++i) {
-        if ((a % i == 0) && (b % i == 0)) 
+        if ((a % i == 0) && (b % i == 0))
             c = i;
     }
     return c;
